da3baff1c8f7358652b59b5263b79e3cd26593c1
tewodrosgetu <tewodrosgetu18@gmail.com>
2025-03-12 22:27:34 -0700

    adding cartslice

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -2,6 +2,7 @@
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 import Button from "../../ui/Button";
+import { useSelector } from "react-redux";
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
@@ -35,7 +36,7 @@ const fakeCart = [
 function CreateOrder() {
   const navigation = useNavigation();
   const isSubmmiting = navigation.state === "submitting";
-
+   const username=useSelector(state=>state.user.username)
   const formError = useActionData();
 
   // const [withPriority, setWithPriority] = useState(false);
@@ -48,7 +49,7 @@ function CreateOrder() {
       <Form method="POST">
         <div className="mb-5 flex flex-col  gap-1 sm:flex-row sm:items-center">
           <label className="sm:basis-40">First Name</label>
-          <input type="text" name="customer" required  className="input  grow"/>
+          <input type="text" name="customer"  required  className="input  grow" defaultValue={username}/>
         </div>
 
         <div  className="mb-5 flex flex-col gap-1 sm:flex-row sm:items-center">
